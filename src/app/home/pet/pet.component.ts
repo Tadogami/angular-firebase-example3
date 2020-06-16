7314e2a9912f6e80c8396e66c8c61627a1d99b85
Naoya Takenami <thriver@imac.local>
2020-06-16 17:13:03 +0900

    feat(home): Petコンポーネント追加

---
+++ src/app/home/pet/pet.component.ts
@@ -0,0 +1,24 @@
+import { Pet } from './../../interfaces/pet';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pet',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet.component.scss']
+})
+export class PetComponent implements OnInit {
+
+  @Input() pet: Pet;
+  maxExp = 400;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getExpPercentage(): number {
+    console.log(this.pet.exp / this.maxExp * 100);
+    return this.pet.exp / this.maxExp * 100;
+  }
+
+}
